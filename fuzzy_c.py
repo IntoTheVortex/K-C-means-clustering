86abf379620a8dd3575a026ac3bc8ff12001decc
IntoTheVortex <amberadelaideshore@gmail.com>
2022-05-27 16:24:57 -0700

    final cleaning

--- fuzzy_c.py
+++ fuzzy_c.py
@@ -5,14 +5,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy.cluster
 
-NUM_CLUSTERS = 5
+NUM_CLUSTERS = 7
 
 def fcm(data):
     ## initialization
 
+    #fuzzifier
     m = 2
     #number of clusters
-    k = 5
+    k = NUM_CLUSTERS 
 
     #initial coefficients/membership grades
     coeffs = np.zeros((len(data), k))
@@ -20,82 +21,74 @@ def fcm(data):
     #init centroids
     max = np.max(data)
     min = np.min(data)
-
     c = np.random.uniform(min, max, [k, 2])
-    #print(c)
 
+
+    r = 10
     ## main loop
-
-    #compute coefficients/membership grades/weights
-    for j in range(k): #j is a cluster
-        # euc dist = sqrt((x1-x2)^2 + (y1-y2)^2)
-        numerator = np.sqrt(
-            np.power(
-                np.subtract(data[:, 0], c[j][0]), 2) 
-            + np.power(
-                np.subtract(data[:, 1], c[j][1]), 2)
-        )
-        print(numerator)
-        #print(numerator.shape)
-        
-        '''trouble getting the order right here'''
-        totals = np.zeros((len(data)))
-        for _k in range(k): # _k is cluster again
-            #coeffs[:, j] = numerator / np.sqrt(
-            totals += numerator / np.sqrt(
+    for _ in range(r):
+        #compute coefficients/membership grades/weights
+        for j in range(k): #j is a cluster
+            # euc dist = sqrt((x1-x2)^2 + (y1-y2)^2)
+            numerator = np.sqrt(
                 np.power(
-                    np.subtract(data[:, 0], c[_k][0]), 2) 
+                    np.subtract(data[:, 0], c[j][0]), 2) 
                 + np.power(
-                    np.subtract(data[:, 1], c[_k][1]), 2)
+                    np.subtract(data[:, 1], c[j][1]), 2)
             )
+            
+            totals = np.zeros((len(data)))
+            for _k in range(k): # _k is cluster again
+                totals += numerator / np.sqrt(
+                    np.power(
+                        np.subtract(data[:, 0], c[_k][0]), 2) 
+                    + np.power(
+                        np.subtract(data[:, 1], c[_k][1]), 2)
+                )
 
-        #print("weights shape:", coeffs[:, j].shape)
-        coeffs[:, j] = 1 / np.power(totals, (2 / (m - 1)))
-        print("weights shape:", coeffs[:, j].shape)
+            coeffs[:, j] = 1 / np.power(totals, (2 / (m - 1)))
 
-        clusters = label_data(data, coeffs)
+        for i in range(k):
+        #compute centroids
+            # fuzzed weights
+            coeff_fuzz = np.power(coeffs[:, i], m)
 
+            # x-vals
+            data_x = np.multiply(coeff_fuzz, data[:, 0])
+            c[i][0] = np.sum(data_x) / np.sum(coeff_fuzz)
 
-    for i in range(k):
-    #compute centroids
-        # fuzzed weights
-        coeff_fuzz = np.power(coeffs[:, i], m)
+            data_y = np.multiply(coeff_fuzz, data[:, 1])
+            c[i][1] = np.sum(data_y) / np.sum(coeff_fuzz)
 
-        # x-vals
-        data_x = np.multiply(coeff_fuzz, data[:, 0])
-        c[i][0] = np.sum(data_x) / np.sum(coeff_fuzz)
-
-        data_y = np.multiply(coeff_fuzz, data[:, 1])
-        c[i][1] = np.sum(data_y) / np.sum(coeff_fuzz)
-    print("c", c)
-
-    clusters = label_data(data, coeffs)
-    plot_data(data, clusters, c)
+        clusters = label_data_cmeans(data, coeffs)
+        plot_data(clusters, c, _, 'c-means')
 
 
 
 
-    '''
-    so we have coefficients for all the data for each class.
-    cluster assignment = max weight/coeff for data i: which class?
-    '''
-def label_data(data, coeffs):
+def label_data_cmeans(data, coeffs):
     clusters = np.zeros((NUM_CLUSTERS, len(data), 2))
     for i in range(len(data)):
         clusters[np.argmax(coeffs[i])][i] = (data[i])
 
     return clusters
 
+def label_data_kmeans(data, centroids):
+    pass
+
 
 
 #plot
-def plot_data(data, clusters, centroids):
-    colors = list("bgrcmyk")
+def plot_data(clusters, centroids, r, filename):
+    colors = list("gbcmky")
 
     for i in range(NUM_CLUSTERS):
-        color = colors[random.randint(0, len(colors)-1)]
+        color = colors[i%len(colors)]
         plt.scatter(clusters[i][:, 0], clusters[i][:, 1], color=color)
 
+    plt.scatter(centroids[:, 0], centroids[:, 1], color='r')
+
+    plt.savefig(filename + str(r) + '.png')
     plt.show()
 
 
@@ -108,7 +101,6 @@ def read_in_data(file_name):
     line = file.readline()
     i = 0
     while(line):
-        #print(line)
         x_y = line.split()
         x_y = [float(x_y[0]), float(x_y[1])]
         data[i][0] = float(x_y[0])
@@ -117,7 +109,6 @@ def read_in_data(file_name):
         i += 1
 
     file.close()
-    print("data", data)
     return data
 
 
