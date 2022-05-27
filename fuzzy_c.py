90ee8c1993c5668ccae689f378ad4323e58b95a4
IntoTheVortex <amberadelaideshore@gmail.com>
2022-05-26 21:15:42 -0700

    plot issues

--- fuzzy_c.py
+++ fuzzy_c.py
@@ -1,10 +1,14 @@
 import sys
 import os.path
+import random
 import numpy as np
+from matplotlib import pyplot as plt
+import scipy.cluster
 
 
 def fcm(data):
     ## initialization
+
     m = 2
     #number of clusters
     k = 5
@@ -22,22 +26,22 @@ def fcm(data):
     ## main loop
 
     #compute coefficients/membership grades/weights
-    for j in range(k):
-        # dist = sqrt((x1-x2)^2 + (y1-y2)^2)
+    for j in range(k): #j is a cluster
+        # euc dist = sqrt((x1-x2)^2 + (y1-y2)^2)
         numerator = np.sqrt(
             np.power(
                 np.subtract(data[:, 0], c[j][0]), 2) 
             + np.power(
                 np.subtract(data[:, 1], c[j][1]), 2)
         )
-        #print(numerator)
+        print(numerator)
         #print(numerator.shape)
         
         '''trouble getting the order right here'''
-        totals = np.zeros((len(data), k))
-        for _k in range(k):
+        totals = np.zeros((len(data)))
+        for _k in range(k): # _k is cluster again
             #coeffs[:, j] = numerator / np.sqrt(
-            totals[_k] += numerator / np.sqrt(
+            totals += numerator / np.sqrt(
                 np.power(
                     np.subtract(data[:, 0], c[_k][0]), 2) 
                 + np.power(
@@ -48,12 +52,11 @@ def fcm(data):
         coeffs[:, j] = 1 / np.power(totals, (2 / (m - 1)))
         print("weights shape:", coeffs[:, j].shape)
 
-    coeffs = 0
-        
-        
+        clusters = label_data(data, coeffs)
+
 
     for i in range(k):
-    #compute centroid
+    #compute centroids
         # fuzzed weights
         coeff_fuzz = np.power(coeffs[:, i], m)
 
@@ -64,6 +67,49 @@ def fcm(data):
         data_y = np.multiply(coeff_fuzz, data[:, 1])
         c[i][1] = np.sum(data_y) / np.sum(coeff_fuzz)
     print("c", c)
+
+    clusters = label_data(data, coeffs)
+    plot_data(data, c)
+
+
+
+
+    '''
+    so we have coefficients for all the data for each class.
+    cluster assignment = max weight/coeff for data i: which class?
+    '''
+def label_data(data, coeffs):
+    clusters = np.zeros(len(data))
+    #for i in range(len(clusters)):
+    for i in range(10):
+        print(coeffs[i])
+        clusters[i] = np.argmax(coeffs[i])
+        print(clusters[i])
+    return clusters
+
+
+
+#plot
+def plot_data(data, centroids):
+    #plt.plot
+    xs = data[:, 0]
+    ys = data[:, 1]
+
+    
+    assignments = scipy.cluster.vq.vq(data, centroids)[0]
+    clusters = [[] for i in range( len( assignments ) )]
+    for item, clustNum in zip( data, assignments ):
+        clusters[clustNum].append( item )
+
+
+    colors = list("bgrcmyk")
+    #num_clusters = max(clusters)
+
+    for i in range(len(clusters)):
+        color = colors[random.randint(0, len(colors))]
+        plt.scatter(clusters[i][:][0], clusters[i][:][0], color=color)
+
+    plt.show()
 
 
 
